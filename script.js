2f1b1aaeb7a2b3a011c56cc613d422ea2c8a66d7
santhraThomas <78120526+santhraThomas@users.noreply.github.com>
2024-07-28 11:52:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,34 @@ const slides = [showTopStudents, showGenderPassRatio, showEthnicityGroups, showT
 
 function showTopStudents() {
     // Load data and create bar chart with top students
+    d3.csv('StudentsPerformance.csv').then(data => {
+        // Process data to find top students
+        // Create bar chart with D3
+        // Add pop-ups and text field for number of students
+    });
 }
-
 function showGenderPassRatio() {
     // Load data and create pie chart with male/female pass ratio
+    d3.csv('StudentsPerformance.csv').then(data => {
+        // Process data to calculate pass ratios
+        // Create pie chart with D3
+    });
 }
 
 function showEthnicityGroups() {
     // Load data and create bar chart with ethnicity groups
+    d3.csv('StudentsPerformance.csv').then(data => {
+        // Process data to group by ethnicity
+        // Create bar chart with D3
+    });
 }
 
 function showTestPreparationStatus() {
     // Load data and create line graph with test preparation status
+    d3.csv('StudentsPerformance.csv').then(data => {
+        // Process data to show test preparation status
+        // Create line graph with D3
+    });
 }
 
 document.getElementById('prev-slide').addEventListener('click', () => {
